2c3ea5eb5a08429100fee7973b1d4be8a971a930
ellai <ellaidevs@gmail.com>
2021-12-17 18:25:02 +0800

    feat: add MUI login page

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 // import logo from './logo.svg'
 import './App.css'
-
+import './login/SignInSide'
+// import Button from '@mui/material/Button'
+import SignInSide from './login/SignInSide'
 function App() {
-  return <div className="App">Hello</div>
+  return (
+    <div className="App">
+      <SignInSide />
+    </div>
+  )
 }
 
 export default App
